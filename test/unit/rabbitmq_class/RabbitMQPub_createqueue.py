d33b4062d4fff4161e38ddfd6d2f5610bc733c53
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-05-22 09:43:17 +0100

    Update test/unit/rabbitmq_class/RabbitMQPub_createqueue.py

--- test/unit/rabbitmq_class/RabbitMQPub_createqueue.py
+++ test/unit/rabbitmq_class/RabbitMQPub_createqueue.py
@@ -78,7 +78,7 @@ class UnitTest(unittest.TestCase):
 
     Methods:
         setUp -> Initialize testing environment.
-        test_open_channel -> Test open_channel method.
+        test_create_queue -> Test create_queue method.
 
     """
 
@@ -103,11 +103,11 @@ class UnitTest(unittest.TestCase):
         self.auto_delete = True
 
     @mock.patch("rabbitmq_class.pika")
-    def test_open_channel(self, mock_pika):
+    def test_create_queue(self, mock_pika):
 
-        """Function:  test_open_channel
+        """Function:  test_create_queue
 
-        Description:  Test open_channel method.
+        Description:  Test create_queue method.
 
         Arguments:
             None
