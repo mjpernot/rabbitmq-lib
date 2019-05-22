c82e77045c63ed263009df06f158bfa893239e21
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-05-22 10:50:53 +0100

    Add test/unit/rabbitmq_class/RabbitMQPub_clearqueue.py

---
+++ test/unit/rabbitmq_class/RabbitMQPub_clearqueue.py
@@ -0,0 +1,126 @@
+#!/usr/bin/python
+# Classification (U)
+
+"""Program:  RabbitMQPub_clearqueue.py
+
+    Description:  Unit test of RabbitMQPub.clear_queue in rabbitmq_class.py.
+
+    Usage:
+        test/unit/rabbitmq_class/RabbitMQPub_clearqueue.py
+
+    Arguments:
+        None
+
+"""
+
+# Libraries and Global Variables
+
+# Standard
+import sys
+import os
+
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
+
+# Third-party
+import mock
+
+# Local
+sys.path.append(os.getcwd())
+import rabbitmq_class
+import version
+
+__version__ = version.__version__
+
+
+class ClearQueue(object):
+
+    """Class:  ClearQueue
+
+    Description:  Class stub holder for pika class.
+
+    Super-Class:  None
+
+    Sub-Classes:  None
+
+    Methods:
+        queue_purge -> Stub holder for queue_purge function.
+
+    """
+
+    def queue_purge(self, queue):
+
+        """Function:  queue_purge
+
+        Description:  Stub holder for queue_purge function.
+
+        Arguments:
+            queue -> Arg stub holder.
+
+        """
+
+        return True
+
+
+class UnitTest(unittest.TestCase):
+
+    """Class:  UnitTest
+
+    Description:  Class which is a representation of a unit testing.
+
+    Super-Class:  unittest.TestCase
+
+    Sub-Classes:  None
+
+    Methods:
+        setUp -> Initialize testing environment.
+        test_clear_queue -> Test clear_queue method.
+
+    """
+
+    def setUp(self):
+
+        """Function:  setUp
+
+        Description:  Initialization for unit testing.
+
+        Arguments:
+            None
+
+        """
+
+        self.name = None
+        self.host = "ServerName"
+        self.port = 5555
+        self.connection = None
+        self.exchange_name = "Exchange_Name"
+        self.queue_name = "Queue_Name"
+        self.routing_key = "Route_Key"
+        self.auto_delete = True
+        self.body = "Message_Body"
+
+    @mock.patch("rabbitmq_class.pika")
+    def test_clear_queue(self, mock_pika):
+
+        """Function:  test_clear_queue
+
+        Description:  Test clear_queue method.
+
+        Arguments:
+            None
+
+        """
+
+        mock_pika.PlainCredentials.return_value = "PlainCredentials"
+        mock_pika.ConnectionParameters.return_value = "ConnectionParameters"
+        mock_pika.BasicProperties.return_value = True
+        rq = rabbitmq_class.RabbitMQPub(self.name, "pwd")
+        rq.channel = ClearQueue()
+
+        self.assertFalse(rq.clear_queue())
+
+
+if __name__ == "__main__":
+    unittest.main()
