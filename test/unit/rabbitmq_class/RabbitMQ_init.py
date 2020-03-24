239fde5b8e22bee7536dea8c40f7ebfcb611f913
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-03-24 10:23:11 +0000

    Update test/unit/rabbitmq_class/RabbitMQ_init.py v1.1.0

--- test/unit/rabbitmq_class/RabbitMQ_init.py
+++ test/unit/rabbitmq_class/RabbitMQ_init.py
@@ -9,7 +9,6 @@
         test/unit/rabbitmq_class/RabbitMQ_init.py
 
     Arguments:
-        None
 
 """
 
@@ -41,10 +40,6 @@ class UnitTest(unittest.TestCase):
 
     Description:  Class which is a representation of a unit testing.
 
-    Super-Class:  unittest.TestCase
-
-    Sub-Classes:  None
-
     Methods:
         setUp -> Initialize testing environment.
         test_with_data -> Test other attributes with data.
@@ -59,7 +54,6 @@ class UnitTest(unittest.TestCase):
         Description:  Initialization for unit testing.
 
         Arguments:
-            None
 
         """
 
@@ -76,7 +70,6 @@ class UnitTest(unittest.TestCase):
         Description:  Test __init__ method with all arguments.
 
         Arguments:
-            None
 
         """
 
@@ -95,7 +88,6 @@ class UnitTest(unittest.TestCase):
         Description:  Test __init__ method with default arguments.
 
         Arguments:
-            None
 
         """
 
