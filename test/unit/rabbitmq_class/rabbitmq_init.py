264e77fe92553a22ebcad088ab87b83b3858a064
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2021-09-28 10:49:27 +0100

    Update test/unit/rabbitmq_class/rabbitmq_init.py v2.1.0

--- test/unit/rabbitmq_class/rabbitmq_init.py
+++ test/unit/rabbitmq_class/rabbitmq_init.py
@@ -41,9 +41,9 @@ class UnitTest(unittest.TestCase):
     Description:  Class which is a representation of a unit testing.
 
     Methods:
-        setUp -> Initialize testing environment.
-        test_with_data -> Test other attributes with data.
-        test_default -> Test with minimum number of arguments.
+        setUp
+        test_with_data
+        test_default
 
     """
 
